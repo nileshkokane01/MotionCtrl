60e94691d9d36c4f23bd9e768185d0b70f10f470
Nilesh Kokane <nilesh.kokane360@gmail.com>
2024-03-06 20:15:00 +0530

    added some more logs

--- utils/utils.py
+++ utils/utils.py
@@ -34,6 +34,7 @@ def instantiate_from_config(config):
         elif config == "__is_unconditional__":
             return None
         raise KeyError("Expected key `target` to instantiate.")
+    print(f'initializating {config['target']}')
     return get_obj_from_str(config["target"])(**config.get("params", dict()))
 
 
