2c236bce4b3e57a024e4f27c9cf43574e285de42
Nilesh Kokane <nilesh.kokane360@gmail.com>
2024-03-06 16:50:13 +0530

    fixed nits

--- configs/inference/run.sh
+++ configs/inference/run.sh
@@ -16,7 +16,7 @@ save_dir=$res_dir/$condtype'_seed'$seed
 
 use_ddp=1
 
-if [ $use_ddp == 0 ]; then
+
 python 'main/evaluation/motionctrl_inference.py' \
 --seed 1234 \
 --ckpt_path $ckpt \
@@ -29,23 +29,4 @@ python 'main/evaluation/motionctrl_inference.py' \
 --ddim_eta 1.0 \
 --condtype $condtype \
 --cond_dir $cond_dir \
-# --save_imgs
-fi
-
-if [ $use_ddp == 1 ]; then
-python3 -m torch.distributed.launch \
---nproc_per_node=3 --nnodes=1 --master_port=23466 \
-main/evaluation/ddp_wrapper.py \
---module 'inference' \
---seed 2000 \
---ckpt_path $ckpt \
---base $config \
---savedir $res_dir/$name \
---n_samples 3 \
---bs 1 --height 256 --width 256 \
---unconditional_guidance_scale 7.5 \
---ddim_steps 50 \
---ddim_eta 1.0 \
---condtype $condtype \
---cond_dir $cond_dir
-fi
+# --save_imgsfi
