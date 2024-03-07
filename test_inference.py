7b0cc7a8c2490b65ed379aea7a257b888157b617
Nilesh Kokane <nilesh.kokane360@gmail.com>
2024-03-07 10:30:29 +0530

    ensure token is loaded

--- test_inference.py
+++ test_inference.py
@@ -3,6 +3,14 @@
 
 import open_clip 
 
+import os
+
+try:
+    # Replace 'your_api_token' with your actual Hugging Face API token
+    os.environ["HF_TOKEN"] = "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"
+except Exception as e:
+    print("Error setting HF_TOKEN:", e)
+
 
 print('attempting to load the open_clip model ')
 model, _, _ = open_clip.create_model_and_transforms('hf-hub:laion/CLIP-ViT-H-14-laion2B-s32B-b79K')
