408b9eeea155bc7934743891fbf54731e597f714
Nilesh Kokane <nilesh.kokane360@gmail.com>
2024-03-07 10:24:56 +0530

    removed unwanted imports

--- test_inference.py
+++ test_inference.py
@@ -1,27 +1,6 @@
-import argparse
-import datetime
-import glob
-import json
-import math
-import os
-import sys
-import time
-from collections import OrderedDict
 
-import cv2
-import numpy as np
-import torch
-import torchvision
-## note: decord should be imported after torch
-from omegaconf import OmegaConf
 
-from tqdm import tqdm
 
-sys.path.insert(1, os.path.join(sys.path[0], '..', '..'))
-from lvdm.models.samplers.ddim import DDIMSampler
-from main.evaluation.motionctrl_prompts_camerapose_trajs import (
-    both_prompt_camerapose_traj, cmcm_prompt_camerapose, omom_prompt_traj)
-from utils.utils import instantiate_from_config
 import open_clip 
 
 
